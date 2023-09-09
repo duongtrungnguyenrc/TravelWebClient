aa3abd5ad59972f093e329fd37cf37bc0b0ad60c
Đường Trung Nguyên <duongtrungnguyenrc@gmail.com>
2023-09-09 20:54:06 +0700

    modify and optimization Bottom

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,13 +1,13 @@
 'use client'
-import Link from "next/link";
+import Bottom from "../_components/Bottom/Bottom";
 import NavBar from "../_components/NavBar/NavBar";
-
 
 export default function Home() {
 
   return (
     <>
       <NavBar/>
+      <Bottom/>
     </>
   )
 }
