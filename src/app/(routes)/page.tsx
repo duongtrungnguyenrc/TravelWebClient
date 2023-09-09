e042b7e3553cc1bd40a16291a44d87c46199921c
Khai Nguyen <cuong.0906140073@gmail.com>
2023-09-09 12:22:51 +0700

    Bottom have reponsive for pc tablet mobile

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,13 +1,14 @@
 'use client'
 import Link from "next/link";
 import NavBar from "../_components/NavBar/NavBar";
-
+import Bottom from "../_components/Bottom/Bottom"
 
 export default function Home() {
 
   return (
     <>
       <NavBar/>
+      <Bottom />
     </>
   )
 }
