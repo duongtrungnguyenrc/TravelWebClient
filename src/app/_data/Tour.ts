49aa12fbb683571ae26625e4d1f021b898144cf3
Đường Trung Nguyên <duongtrungnguyenrc@gmail.com>
2023-09-13 17:01:27 +0700

    test with fake data

---
+++ src/app/_data/Tour.ts
@@ -0,0 +1,13 @@
+import { Tour } from "../_types";
+
+const Tour : Tour = {
+    img : "/images/home-slider-3.jpg",
+    name : "Deluxe Double with New York City View",
+    price : 59,
+    ratedStar : 4.9,
+    location : "Phu Quy",
+    maxPeople : 3,
+    time : 3,
+}
+
+export default Tour;
---
+++ src/app/_data/TourList.ts
@@ -0,0 +1,8 @@
+import { Tour as TourType } from "../_types";
+import Tour from "./Tour";
+
+const TourList : TourType[] =  [
+    Tour, Tour, Tour, Tour, Tour, Tour, Tour, Tour
+]
+
+export default TourList;
