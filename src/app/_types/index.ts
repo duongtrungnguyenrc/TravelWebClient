83fde09380eb722bf8243b2ab129825355033000
Đường Trung Nguyên <duongtrungnguyenrc@gmail.com>
2023-09-17 18:53:31 +0700

    change response type import path

--- src/app/_types/index.ts
+++ src/app/_types/index.ts
@@ -1,4 +1,4 @@
 export type { default as Tour } from "./Tour";
 export type { default as Weather } from "./Weather";
-export type { default as Response } from "./response";
+export type { default as Response } from "./Response";
 export type { default as Route } from "./Route";
